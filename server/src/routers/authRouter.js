86c6f9b39f00e93be1f830dcf9b09fc9b68b714f
Ajmir Raziqi <ajmir.ng3@gmail.com>
2024-02-13 10:36:38 +0200

    add validators to auth router

--- server/src/routers/authRouter.js
+++ server/src/routers/authRouter.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const adapt = require("../utils/adapt");
 const { authController } = require("../controllers");
-const { userMiddleware } = require("../middlewares");
+const { userMiddleware, sharedMiddleware } = require("../middlewares");
+const userValidator = require("../validators/userValidator");
 
 const authRouter = express.Router();
 
 // --- authRouter
 authRouter.get("/", adapt(authController.getAuth));
-authRouter.post("/sign-in", adapt(authController.signIn));
+authRouter.post(
+  "/sign-in",
+  adapt(
+    sharedMiddleware.validateBodyMiddlewareBuilder(
+      userValidator.pick({ email: true, password: true })
+    ),
+    authController.signIn
+  )
+);
 authRouter.post(
   "/sign-up",
   adapt(
+    sharedMiddleware.validateBodyMiddlewareBuilder(
+      userValidator.pick({ email: true, password: true })
+    ),
     userMiddleware.isEmailUnique,
     userMiddleware.hashPassword,
     authController.signUp
