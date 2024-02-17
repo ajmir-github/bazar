08d11f48f2d446ac969be2a3ff852d83a916dd00
Ajmir Raziqi <ajmir.ng3@gmail.com>
2024-02-17 14:25:54 +0200

    sync search params with listings filter options

--- client/src/components/ListingsOptions.tsx
+++ client/src/components/ListingsOptions.tsx
@@ -7,109 +7,178 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { ArrowUpIcon } from "lucide-react";
-import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Button } from "./ui/button";
+import { FilterIcon } from "lucide-react";
+import { useEffect } from "react";
 
 const categories = [];
 
+const FormSchema = z.object({
+  category: z.string(),
+  condition: z.string(),
+  minPrice: z.string().optional(),
+  maxPrice: z.string().optional(),
+  sort: z.string(),
+});
 export default function ListingsOptions() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      condition: "*",
+      category: "*",
+      sort: "date::asc",
+    },
+  });
+
+  useEffect(() => {
+    const query = Object.fromEntries(searchParams.entries());
+    form.reset(query);
+  }, [searchParams]);
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    // const onlyDefinedInputs = JSON.parse(JSON.stringify(data));
+    const onlyDefinedInputs = Object.fromEntries(
+      Object.entries(data).filter(([_, value]) => !(!value || value === "*"))
+    );
+    setSearchParams(onlyDefinedInputs);
+  }
+
   return (
-    <div className="p-2 gap-2 grid grid-cols-2 md:grid-cols-6">
-      <Select>
-        <SelectTrigger className="col-span-2">
-          <SelectValue placeholder="Select a timezone" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>North America</SelectLabel>
-            <SelectItem value="est">Eastern Standard Time (EST)</SelectItem>
-            <SelectItem value="cst">Central Standard Time (CST)</SelectItem>
-            <SelectItem value="mst">Mountain Standard Time (MST)</SelectItem>
-            <SelectItem value="pst">Pacific Standard Time (PST)</SelectItem>
-            <SelectItem value="akst">Alaska Standard Time (AKST)</SelectItem>
-            <SelectItem value="hst">Hawaii Standard Time (HST)</SelectItem>
-          </SelectGroup>
-          <SelectGroup>
-            <SelectLabel>Europe & Africa</SelectLabel>
-            <SelectItem value="gmt">Greenwich Mean Time (GMT)</SelectItem>
-            <SelectItem value="cet">Central European Time (CET)</SelectItem>
-            <SelectItem value="eet">Eastern European Time (EET)</SelectItem>
-            <SelectItem value="west">
-              Western European Summer Time (WEST)
-            </SelectItem>
-            <SelectItem value="cat">Central Africa Time (CAT)</SelectItem>
-            <SelectItem value="eat">East Africa Time (EAT)</SelectItem>
-          </SelectGroup>
-          <SelectGroup>
-            <SelectLabel>Asia</SelectLabel>
-            <SelectItem value="msk">Moscow Time (MSK)</SelectItem>
-            <SelectItem value="ist">India Standard Time (IST)</SelectItem>
-            <SelectItem value="cst_china">China Standard Time (CST)</SelectItem>
-            <SelectItem value="jst">Japan Standard Time (JST)</SelectItem>
-            <SelectItem value="kst">Korea Standard Time (KST)</SelectItem>
-            <SelectItem value="ist_indonesia">
-              Indonesia Central Standard Time (WITA)
-            </SelectItem>
-          </SelectGroup>
-          <SelectGroup>
-            <SelectLabel>Australia & Pacific</SelectLabel>
-            <SelectItem value="awst">
-              Australian Western Standard Time (AWST)
-            </SelectItem>
-            <SelectItem value="acst">
-              Australian Central Standard Time (ACST)
-            </SelectItem>
-            <SelectItem value="aest">
-              Australian Eastern Standard Time (AEST)
-            </SelectItem>
-            <SelectItem value="nzst">
-              New Zealand Standard Time (NZST)
-            </SelectItem>
-            <SelectItem value="fjt">Fiji Time (FJT)</SelectItem>
-          </SelectGroup>
-          <SelectGroup>
-            <SelectLabel>South America</SelectLabel>
-            <SelectItem value="art">Argentina Time (ART)</SelectItem>
-            <SelectItem value="bot">Bolivia Time (BOT)</SelectItem>
-            <SelectItem value="brt">Brasilia Time (BRT)</SelectItem>
-            <SelectItem value="clt">Chile Standard Time (CLT)</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="p-2 gap-2 grid grid-cols-2 md:grid-cols-6 items-end"
+      >
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="*">Any</SelectItem>
+                  <SelectItem value="Electronics">Electronics</SelectItem>
+                  <SelectItem value="Clothes">Clothes</SelectItem>
+                  <SelectItem value="Vahicles">Vahicles</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="condition"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Condition</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Conidtion" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="*">Any</SelectItem>
+                  <SelectItem value="date::assc">New</SelectItem>
+                  <SelectItem value="price::aasdsc">barely used</SelectItem>
+                  <SelectItem value="date::desdac">Used</SelectItem>
+                  <SelectItem value="price:deswc">Old</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <Input type="number" placeholder="Min Price" />
-      <Input type="number" placeholder="Max Price" />
+        <FormField
+          control={form.control}
+          name="minPrice"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Min Price</FormLabel>
+              <Input
+                type="number"
+                defaultValue={field.value}
+                onChange={field.onChange}
+              />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="maxPrice"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Max Price</FormLabel>
+              <Input
+                type="number"
+                defaultValue={field.value}
+                onChange={field.onChange}
+              />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-      <Select defaultValue="*">
-        <SelectTrigger className="">
-          <SelectValue placeholder="Condition" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="*">Any</SelectItem>
-          <SelectItem value="date::assc">New</SelectItem>
-          <SelectItem value="price::aasdsc">barely used</SelectItem>
-          <SelectItem value="date::desdac">Used</SelectItem>
-          <SelectItem value="price:deswc">Old</SelectItem>
-        </SelectContent>
-      </Select>
-      <Select defaultValue="date::asc">
-        <SelectTrigger className="">
-          <SelectValue placeholder="Sort" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Sort by date</SelectLabel>
-            <SelectItem value="date::asc">Sort Newest</SelectItem>
-            <SelectItem value="date::desc">Sort Oldest</SelectItem>
-          </SelectGroup>
-          <SelectGroup>
-            <SelectLabel>Sort by price</SelectLabel>
-            <SelectItem value="price::asc">Sort Heighest</SelectItem>
-            <SelectItem value="price:desc">Sort Lowest</SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-    </div>
+        <FormField
+          control={form.control}
+          name="sort"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Sort</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Sort" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Sort by date</SelectLabel>
+                    <SelectItem value="date::asc">Sort Newest</SelectItem>
+                    <SelectItem value="date::desc">Sort Oldest</SelectItem>
+                  </SelectGroup>
+
+                  <SelectGroup>
+                    <SelectLabel>Sort by price</SelectLabel>
+                    <SelectItem value="price::asc">Sort Heighest</SelectItem>
+                    <SelectItem value="price:desc">Sort Lowest</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" className="gap-1">
+          <FilterIcon size={16} />
+          Filter
+        </Button>
+      </form>
+    </Form>
   );
 }
